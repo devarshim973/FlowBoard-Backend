2064ba48a326254d0b365719d4ee9b35881abe1d
devarshim973 <devarshim973@gmail.com>
2026-04-21 11:26:23 +0530

    [Devarshi]: implement Eureka-server

---
+++ Eureka-server/src/test/java/com/flowboard/Eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowboard.Eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
